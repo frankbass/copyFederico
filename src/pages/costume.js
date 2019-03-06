58e0f413a9c6b257024b5c6db9f54c44c90fac93
frankbass <36844912+frankbass@users.noreply.github.com>
2019-03-06 20:23:09 +0100

    first commit

---
+++ src/pages/costume.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Image from "../components/image"
+
+const costumePage = () => (
+  <Layout>
+    <SEO title="costume" />
+    <h1>Costume Design</h1>
+    <div className="pagecontent" style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default costumePage
---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+
+const IndexPage = () => (
+
+
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+
+    <h1>Some awesome!</h1>
+    <div className="topnav">
+
+      <Link className="button" to="/costume/">costume design</Link>
+
+      <Link className="button" to="/art/">art work</Link>
+
+      <Link className="button" to="/about/">about</Link>
+    </div>
+
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+
+
+
+
+
+  </Layout>
+)
+
+export default IndexPage
